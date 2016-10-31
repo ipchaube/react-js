574b45a9dd11841ab60e69440a5a49545a9495c2
Indu Prakash <prakash.indu@gmail.com>
2016-10-31 12:32:39 -0700

    JSX example with remarkable template rendering

--- app/component.js
+++ app/component.js
@@ -13,13 +13,51 @@ var HeaderBox = React.createClass({displayName: 'HeaderBox',
         );
     }
 });
-
+/*
 var CommentBox = React.createClass({displayName: 'CommentBox',
     render: function() {
         return (
             React.createElement('div', {className: "commentBox"},
                 "Hello, world! I am a CommentBox."
             )
+        );
+    }
+});
+*/
+
+
+var data = [
+    { id: 1, author: "Daniel Lo Nigro", text: "Hello ReactJS.NET World!" },
+    { id: 2, author: "Pete Hunt", text: "This is one comment" },
+    { id: 3, author: "Jordan Walke", text: "This is *another* comment" }
+];
+
+
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.props.data} />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+            {comment.text}
+            </Comment>
+            );
+        });
+        return (
+            <div className="commentList">
+            {commentNodes}
+            </div>
         );
     }
 });
